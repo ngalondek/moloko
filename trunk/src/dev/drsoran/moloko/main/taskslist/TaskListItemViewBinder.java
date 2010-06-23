cec6f18a85c617e034f486144dc978b6c02a24ee
ronny.roehricht <ronny.roehricht@611209e8-4120-9e6d-523c-38642cc5b603>
2010-06-23 18:03:30 +0000

    - use of ContentUiMapper

--- trunk/src/dev/drsoran/moloko/main/taskslist/TaskListItemViewBinder.java
+++ trunk/src/dev/drsoran/moloko/main/taskslist/TaskListItemViewBinder.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.graphics.Typeface;
 import android.text.SpannableString;
 import android.text.format.DateFormat;
+import android.text.format.DateUtils;
 import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.Button;
@@ -17,53 +18,67 @@ import android.widget.SimpleCursorAdapter.ViewBinder;
 import com.mdt.rtm.data.RtmTask;
 
 import dev.drsoran.moloko.R;
+import dev.drsoran.moloko.util.ContentUiMapper;
+import dev.drsoran.provider.Rtm.Tasks;
 
 
 public final class TaskListItemViewBinder implements ViewBinder
 {
    private final ListActivity context;
    
+   private final int DESCRIPTION;
+   
+   private final int DUE_DATE;
+   
+   private final int PRIORITY;
+   
+   private final int COMPLETED;
+   
    
 
-   public TaskListItemViewBinder( ListActivity context ) throws NullPointerException
+   public TaskListItemViewBinder( ListActivity context, ContentUiMapper mapper )
+      throws NullPointerException
    {
-      if ( context == null )
+      if ( context == null || mapper == null )
          throw new NullPointerException();
       
       this.context = context;
+      this.DESCRIPTION = mapper.UI_COL_INDICES.get( Tasks.TASKSERIES_NAME );
+      this.DUE_DATE = mapper.UI_COL_INDICES.get( Tasks.DUE_DATE );
+      this.PRIORITY = mapper.UI_COL_INDICES.get( Tasks.PRIORITY );
+      this.COMPLETED = mapper.UI_COL_INDICES.get( Tasks.COMPLETED_DATE );
    }
    
 
 
    public boolean setViewValue( View view, Cursor cursor, int columnIndex )
    {
-      switch ( columnIndex )
+      
+      if ( columnIndex == DESCRIPTION )
       {
-         // TODO: Make these magic numbers constants.
-         // +1 due to _id
-         case 1:
-            boolean handled = false;
+         boolean handled = false;
+         
+         // description
+         if ( !cursor.isNull( DUE_DATE ) )
+         {
+            final long dueDateMs = cursor.getLong( DUE_DATE );
             
-            // description
-            if ( !cursor.isNull( 3 ) /* due date */)
+            // Make bold of the task is today
+            if ( DateUtils.isToday( dueDateMs ) )
             {
-               final Date dueDate = new Date( cursor.getLong( 3 ) );
-               final Date now = new Date();
+               final TextView taskDesc = (TextView) view;
+               taskDesc.setTypeface( Typeface.DEFAULT_BOLD );
+               taskDesc.setText( cursor.getString( columnIndex ) );
                
-               // Make bold of the task is today
-               if ( now.getYear() == dueDate.getYear()
-                  && now.getMonth() == dueDate.getMonth()
-                  && now.getDay() == dueDate.getDay() )
-               {
-                  final TextView taskDesc = (TextView) view;
-                  taskDesc.setTypeface( Typeface.DEFAULT_BOLD );
-                  taskDesc.setText( cursor.getString( columnIndex ) );
-                  
-                  handled = true;
-               }
+               handled = true;
+            }
+            
+            // Make underline if overdue
+            else
+            {
+               final Date dueDate = new Date( dueDateMs );
                
-               // Make underline if overdue
-               else if ( now.after( dueDate ) )
+               if ( new Date().after( dueDate ) )
                {
                   final TextView taskDesc = (TextView) view;
                   final SpannableString content = new SpannableString( cursor.getString( columnIndex ) );
@@ -74,55 +89,62 @@ public final class TaskListItemViewBinder implements ViewBinder
                   handled = true;
                }
             }
-            
-            return handled;
-            
-         case 3:
-            // due date
-            final TextView dueField = (TextView) view;
-            
-            if ( !cursor.isNull( columnIndex ) )
-            {
-               dueField.setText( DateFormat.getDateFormat( context )
-                                           .format( new Date( cursor.getLong( columnIndex ) ) ) );
-            }
-            else
-               dueField.setText( "" );
-            
-            return true;
-            
-         case 4:
-            // priority
-            final Button prioBar = (Button) view;
-            
-            switch ( RtmTask.convertPriority( cursor.getString( columnIndex ) ) )
-            {
-               case High:
-                  prioBar.setBackgroundResource( R.color.priority_1 );
-                  return true;
-               case Medium:
-                  prioBar.setBackgroundResource( R.color.priority_2 );
-                  return true;
-               case Low:
-                  prioBar.setBackgroundResource( R.color.priority_3 );
-                  return true;
-               case None:
-               default :
-                  prioBar.setBackgroundResource( R.color.priority_none );
-                  return true;
-            }
-            
-         case 5:
-            // completed
-            final CheckBox checkBox = (CheckBox) view;
-            checkBox.setChecked( !cursor.isNull( columnIndex ) );
-            
-            return true;
-            
-         default :
-            // SimpleCursorAdapter will bind
-            return false;
+         }
+         
+         return handled;
       }
+      
+      else if ( columnIndex == DUE_DATE )
+      {
+         // due date
+         final TextView dueField = (TextView) view;
+         
+         if ( !cursor.isNull( columnIndex ) )
+         {
+            dueField.setText( DateFormat.getDateFormat( context )
+                                        .format( new Date( cursor.getLong( columnIndex ) ) ) );
+         }
+         else
+            dueField.setText( "" );
+         
+         return true;
+      }
+      
+      else if ( columnIndex == PRIORITY )
+      {
+         // priority
+         final Button prioBar = (Button) view;
+         
+         switch ( RtmTask.convertPriority( cursor.getString( columnIndex ) ) )
+         {
+            case High:
+               prioBar.setBackgroundResource( R.color.priority_1 );
+               return true;
+            case Medium:
+               prioBar.setBackgroundResource( R.color.priority_2 );
+               return true;
+            case Low:
+               prioBar.setBackgroundResource( R.color.priority_3 );
+               return true;
+            case None:
+            default :
+               prioBar.setBackgroundResource( R.color.priority_none );
+               return true;
+         }
+      }
+      
+      else if ( columnIndex == COMPLETED )
+      {
+         // completed
+         final CheckBox checkBox = (CheckBox) view;
+         checkBox.setChecked( !cursor.isNull( columnIndex ) );
+         
+         return true;
+      }
+      
+      else
+         // SimpleCursorAdapter will bind
+         return false;
    }
    
 }

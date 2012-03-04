4f85b950ad4c776b5ffa35c2515abf4f15184b09
ronny.roehricht <ronny.roehricht@gmail.com>
2012-03-04 18:58:10 +0000

    - added API8 notification icons assets

---
+++ assets/_pre_production/ic_notify/api8/ic_notify.jsx
@@ -0,0 +1,17 @@
+﻿prefix = trimPrefixLayers( prefix, 1 );
+
+if ( selectLayer( activeDocument, "text" ) )
+{
+   setForegroundColor( 0.0, 0.0, 0.0 );
+   
+   for ( var j = 1; j < 10; ++j )
+   {
+      setText( "" + j );
+      suffix = "_" + j;
+      doExport(true, false, false);
+   }
+
+   alreadyExported = true;
+}
+
+

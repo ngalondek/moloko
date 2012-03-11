934ffd9fa214de4a9fb7b23af4e5fc3808fbb1e6
ronny.roehricht <ronny.roehricht@611209e8-4120-9e6d-523c-38642cc5b603>
2012-03-11 10:04:57 +0000

    - added Android style guideline confirm notification icons

---
+++ assets/_pre_production/ic_notify/#v11/ic_notify_v11.jsx
@@ -0,0 +1,23 @@
+﻿ldpiFolder         = preProdFolder + "/../../res/drawable-ldpi-v11";
+mdpiFolder 	 = preProdFolder + "/../../res/drawable-mdpi-v11";
+hdpiFolder	   = preProdFolder + "/../../res/drawable-hdpi-v11";
+xhdpiFolder	 = preProdFolder + "/../../res/drawable-xhdpi-v11";
+
+if ( selectLayer( activeDocument, "text" ) )
+{
+   for ( var j = 1; j < 10; ++j )
+   {
+      setText( "" + j, 255.0, 255.0, 255.0, 11.0 );
+      suffix = "_" + j;
+      
+      doExport(file,
+                    drawableFolder,
+                    ldpiFolder,
+                    mdpiFolder,
+                    hdpiFolder,
+                    xhdpiFolder,
+                    prefix, suffix, false);
+   }
+
+   alreadyExported = true;
+}

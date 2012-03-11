0bbf26e990801c83e67625dfb3f95e7816931041
ronny.roehricht <ronny.roehricht@611209e8-4120-9e6d-523c-38642cc5b603>
2012-03-11 17:29:31 +0000

    - added missing v9 notification icons

---
+++ branches/dev_0.17.0b/assets/_pre_production/ic_notify/#v9/#xhdpi/ic_notify_v9_xhdpi.jsx
@@ -0,0 +1,20 @@
+﻿xhdpiFolder = preProdFolder + "/../../res/drawable-xhdpi-v9";
+
+resizeImagePx( 32, 50 );
+
+if ( selectLayer( activeDocument, "text" ) )
+{
+   for ( var j = 1; j < 10; ++j )
+   {
+      setText( "" + j, 130.0, 130.0, 130.0, 20.0 );
+      suffix = "_" + j;
+      
+      exportXHDPI( file, xhdpiFolder, prefix, suffix, true );
+   }
+}
+else
+{    
+    exportXHDPI( file, xhdpiFolder, prefix, "", true );
+}
+
+alreadyExported = true;

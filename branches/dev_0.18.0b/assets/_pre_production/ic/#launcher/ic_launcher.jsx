8b4862c3d1b68cf2ef1c81f1faa7db0c737d711a
ronny.roehricht@gmail.com <ronny.roehricht@gmail.com@611209e8-4120-9e6d-523c-38642cc5b603>
2014-03-21 14:04:08 +0000

    - fixed export scripts

--- branches/dev_0.18.0b/assets/_pre_production/ic/#launcher/ic_launcher.jsx
+++ branches/dev_0.18.0b/assets/_pre_production/ic/#launcher/ic_launcher.jsx
@@ -1,6 +1,4 @@
-ldpiFolder   = preProdFolder + "/../../res/drawable-ldpi-v11";
-mdpiFolder 	 = preProdFolder + "/../../res/drawable-mdpi-v11";
-hdpiFolder	 = preProdFolder + "/../../res/drawable-hdpi-v11";
-xhdpiFolder	 = preProdFolder + "/../../res/drawable-xhdpi-v11";
-
-filename = filename.replace("_v11", "");
+﻿ldpiFolder   = preProdFolder + "/../../res/drawable-ldpi";
+mdpiFolder 	 = preProdFolder + "/../../res/drawable-mdpi";
+hdpiFolder	 = preProdFolder + "/../../res/drawable-hdpi";
+xhdpiFolder	 = preProdFolder + "/../../res/drawable-xhdpi";
